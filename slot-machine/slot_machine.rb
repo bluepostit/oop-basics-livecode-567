ba9ec280f77ddba802ae8e02acd2764adb2e462c
Yair <32864499+bluepostit@users.noreply.github.com>
2021-01-27 17:21:32 +0200

    completed slot machine

--- slot-machine/slot_machine.rb
+++ slot-machine/slot_machine.rb
@@ -9,10 +9,24 @@ class SlotMachine
     reels = [@reel1, @reel2, @reel3]
     return 0 unless @reel1 == @reel2 || @reel1 == @reel3 || @reel2 == @reel3
 
+    # 3 of the same
     return 50 if reels.count('joker') == 3
     return 40 if reels.count('star') == 3
     return 30 if reels.count('bell') == 3
     return 20 if reels.count('seven') == 3
     return 10 if reels.count('cherry') == 3
+
+    # 2 jokers with anything
+    # (3 jokers was dealt with above)
+    return 25 if reels.count('joker') == 2
+
+    # Exclude jokers from the rest of '2 of anything':
+    return 0 unless reels.include?('joker')
+
+    # 2 of anything, without a joker
+    return 20 if reels.count('star') == 2
+    return 15 if reels.count('bell') == 2
+    return 10 if reels.count('seven') == 2
+    return 5 if reels.count('cherry') == 2
   end
 end
--- slot-machine/spec/slot_machine_spec.rb
+++ slot-machine/spec/slot_machine_spec.rb
@@ -10,10 +10,33 @@ end
 
 describe SlotMachine do
   describe '#score' do
+    # 3 of the same
     test_scenario(%w[joker joker joker], 50)
     test_scenario(%w[star star star], 40)
     test_scenario(%w[bell bell bell], 30)
     test_scenario(%w[seven seven seven], 20)
     test_scenario(%w[cherry cherry cherry], 10)
+
+    # 2 jokers
+    test_scenario(%w[joker joker star], 25)
+    test_scenario(%w[joker joker bell], 25)
+    test_scenario(%w[joker seven joker], 25)
+    test_scenario(%w[cherry joker joker], 25)
+
+    # 2 of the same + joker
+    test_scenario(%w[star joker star], 20)
+    test_scenario(%w[joker bell bell], 15)
+    test_scenario(%w[seven joker seven], 10)
+    test_scenario(%w[cherry cherry joker], 5)
+
+    # 2 of the same + non-joker - zero score!
+    test_scenario(%w[star bell star], 0)
+    test_scenario(%w[seven bell bell], 0)
+    test_scenario(%w[seven star seven], 0)
+    test_scenario(%w[cherry cherry bell], 0)
+
+    # All different - zero score!
+    test_scenario(%w[star bell cherry], 0)
+    test_scenario(%w[seven cherry bell], 0)
   end
 end
